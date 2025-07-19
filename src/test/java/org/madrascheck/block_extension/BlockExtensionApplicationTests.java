aa3a22360326959ba82fc7d2135da792a19a60b2
JIN-076 <andy230@naver.com>
2025-07-19 14:34:42 +0900

    INIT : create project

---
+++ src/test/java/org/madrascheck/block_extension/BlockExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.madrascheck.block_extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
